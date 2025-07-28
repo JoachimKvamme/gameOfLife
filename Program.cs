5e163afb9b702562a2f7b973bd77624e5b197d83
Joachim Åsen Kvamme <150397418+JoachimKvamme@users.noreply.github.com>
2025-07-28 15:06:08 +0200

    prøver å finne en måte å kutte vekk ledende (og haltende(?)) nuller fra arrayet på.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using csharp_codewars2.Classes;
+using System.Linq;
 
 namespace csharp_codewars2;
 
@@ -9,5 +10,39 @@ class Program
         Console.WriteLine("Hello, World!");
         ConwayLifeKata gameOfLife = new ConwayLifeKata();
         gameOfLife.ConwayLife(gameOfLife.matrix, 0);
+
+        List<List<int>> ints =
+        [[0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 1, 1, 0, 0, 0, 0]];
+        int firstIndex = ints[0].Count() -1;
+        int lastIndex = ints[0].Count() - 1;
+        foreach (var list in ints)
+        {
+            if (list.IndexOf(1) < firstIndex)
+            {
+                firstIndex = list.IndexOf(1);
+            }
+            if (list.LastIndexOf(1) < lastIndex)
+            {
+                lastIndex = list.LastIndexOf(1);
+            }
+
+        }
+        Console.WriteLine(firstIndex);
+        Console.WriteLine(lastIndex);
+        
+
+        string showints = "";
+        foreach (var item in ints)
+        {
+            foreach (var cell in item)
+            {
+                showints += cell;
+            }
+            showints += "\n";
+        }
+
+        Console.WriteLine(showints);
+
     }
 }
