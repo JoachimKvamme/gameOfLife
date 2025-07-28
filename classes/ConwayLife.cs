af749480ea2be5b3bc0e298b12b5382ecc0de728
Joachim Åsen Kvamme <150397418+JoachimKvamme@users.noreply.github.com>
2025-07-28 10:47:04 +0200

    Løkker?

--- classes/ConwayLife.cs
+++ classes/ConwayLife.cs
@@ -5,8 +5,32 @@ using System.Threading.Tasks;
 
 namespace csharp_codewars2.Classes
 {
-    public class ConwayLife
+    public class ConwayLifeKata
     {
+
+        List<List<int>> matrix =
+        [[0, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 0,]];
+
+        public List<List<int>> ConwayLife(List<List<int>> matrix)
+        {
+            for (int row = 0; row < matrix.Count(); row++)
+            {
+                for (int field = 0; field < matrix[row].Count(); field++)
+                {
+                    if (matrix[row][field] == 1)
+                    {
+                        
+                    }
+                }
+            }
+
+            return [[]];
+        }
+
         
+
     }
 }
